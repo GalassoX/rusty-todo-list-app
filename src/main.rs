a4276d5590108ce80606c9a319c900dd623cbc0c
Rafael <68511324+GalassoX@users.noreply.github.com>
2022-10-19 17:16:13 -0500

    add: default file if not defined

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 mod cli;
 mod task;
+use anyhow::anyhow;
+use cli::{Action::*, CommandLineArgs};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
-use cli::{Action::*, CommandLineArgs};
+fn find_default_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rust-todo-list.json");
+        path
+    })
+}
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let CommandLineArgs {
         action,
         journal_file,
     } = CommandLineArgs::from_args();
 
-    let journal_file = journal_file.expect("Failed to find journal file");
+    let journal_file = journal_file
+        .or_else(find_default_file)
+        .ok_or(anyhow!("Failed to find journal file"))?;
 
     match action {
         Add { text } => task::add_task(journal_file, task::Task::new(text)),
         Done { position } => task::complete_task(journal_file, position),
         List => task::list_task(journal_file),
-    }
-    .expect("Failed to perform action")
+    }?;
+    Ok(())
 }
